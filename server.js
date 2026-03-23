afbd8a5596f63f0fd64c70a86f03bf98202cd961
Md Hasan Mahmud Rimon <hanmadrin@gmail.com>
2026-03-23 18:41:18 +0600

    server front from ackedn

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 import express, { json, urlencoded } from 'express';
 import cors from 'cors';
-const app = express();
-import ExpressError from './utilities/expressError.js';
-// const { connectToDatabase } = require('./config/config');
-import frontendApi from './routers/frontendApi.js';
-const port = process.env.PORT || 4566;
-app.use(cors());
-app.use(json({limit: '50mb'}));
-app.use(urlencoded({limit: '50mb',extended: true }));
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const app = express();
+
+// FIX __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import ExpressError from './utilities/expressError.js';
+import frontendApi from './routers/frontendApi.js';
+
+const port = process.env.PORT || 4566;
+
+app.use(cors());
+app.use(json({ limit: '50mb' }));
+app.use(urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/api', frontendApi);
-app.listen(port, "0.0.0.0",() => {
-    console.log(`server running at http://localhost:${port}`)
-});
 
-// connectToDatabase();
+app.use(express.static(path.join(__dirname, '../marketShareFrontend/dist')));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../marketShareFrontend/dist', 'index.html'));
+});
+app.listen(port, "0.0.0.0", () => {
+  console.log(`server running at http://localhost:${port}`);
+});
